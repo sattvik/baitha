0b664f83550475454a28714ffa9f4f368772588c
Daniel Solano Gómez <daniel@solanogomez.org>
2011-05-29 18:13:47 -0500

    Prepare for IDEA plugin

--- project/plugins/plugins.scala
+++ project/plugins/plugins.scala
@@ -2,4 +2,7 @@ import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val android = "org.scala-tools.sbt" % "sbt-android-plugin" % "0.5.2-SNAPSHOT"
+
+  val sbtIdeaRepo = "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
+  val sbtIdea  = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.4.0"
 }
