79cf46b3fbcc06e81e273c57f5ce0ce24a099e13
Daniel Solano Gómez <daniel@solanogomez.org>
2011-05-29 18:56:19 -0500

    Point to publicly-available build of customised plugin

--- project/plugins/plugins.scala
+++ project/plugins/plugins.scala
@@ -1,7 +1,8 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val android = "org.scala-tools.sbt" % "sbt-android-plugin" % "0.5.2-SNAPSHOT"
+  val androidRepo = "sattvik-android-repo" at "http://sattvik.github.com/maven/"
+  val android = "com.sattvik" % "sbt-android-plugin" % "0.5.2-SNAPSHOT"
 
   val sbtIdeaRepo = "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
   val sbtIdea  = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.4.0"
