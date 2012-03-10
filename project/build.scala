6987401abe0e2ea950b91d4744880a79f69ee7d0
Daniel Solano Gómez <daniel@solanogomez.org>
2012-03-10 15:29:11 -0600

    Some nonindexed stuff

--- project/build.scala
+++ project/build.scala
@@ -6,7 +6,7 @@ import AndroidKeys._
 object BaithaDefaults {
   val settings =
     Defaults.defaultSettings ++
-    AndroidProject.androidSettings ++ Seq (
+    AndroidBase.settings ++ Seq (
       name := "Baitha: The Scala/Android Toolkit",
       organization := "com.sattvik",
       version := "0.1-SNAPSHOT",
@@ -27,6 +27,7 @@ object BaithaBuild extends Build {
     "baitha",
     file("."),
     settings = BaithaDefaults.settings ++ Seq (
+      name := "baitha",
       libraryDependencies ++= Seq (
         "org.scalatest" %% "scalatest" % "1.7.1" % "test",
         "org.mockito" % "mockito-core" % "1.9.0" % "test"
@@ -34,8 +35,15 @@ object BaithaBuild extends Build {
     )
   )
 
-  //lazy val test = Project(
-    //"baitha-test",
-
-  //)
+  lazy val test = Project(
+    "baitha-test",
+    file("test"),
+    settings =
+      BaithaDefaults.settings ++
+      AndroidTest.androidSettings ++
+      AndroidTest.settings ++ Seq (
+        name := "baitha_test",
+        libraryDependencies += "org.scalatest" %% "scalatest" % "1.7.1" % "test"
+      )
+  ) dependsOn main
 }
