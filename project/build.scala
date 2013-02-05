f95e245c76d8598c4a6338af35a747e8bff6bca7
Daniel Solano Gómez <daniel@solanogomez.org>
2013-02-05 09:17:52 -0600

    Update build to Scala 2.10.0 and Android 17

--- project/build.scala
+++ project/build.scala
@@ -10,13 +10,13 @@ object BaithaDefaults {
       name := "Baitha: The Scala/Android Toolkit",
       organization := "com.sattvik",
       version := "0.1.0-SNAPSHOT",
-      scalaVersion := "2.9.2",
+      scalaVersion := "2.10.0",
       scalacOptions ++= Seq (
         Opts.compile.deprecation,
         Opts.compile.unchecked,
         Opts.compile.optimise
       ),
-      platformName in Android := "android-15"
+      platformName in Android := "android-17"
     )
 }
 
@@ -26,13 +26,18 @@ object BaithaBuild extends Build {
     file("."),
     settings = BaithaDefaults.settings ++ Seq (
       name := "baitha",
-      libraryDependencies ++= Seq (
-        "org.scalatest" %% "scalatest" % "1.7.1" % "test",
-        "org.mockito" % "mockito-core" % "1.9.0" % "test"
-      ),
+      libraryDependencies <+= scalaVersion(scalatestDependency(_)),
+      libraryDependencies += "org.mockito" % "mockito-core" % "1.9.5" % "test",
       crossScalaVersions := Seq(
-        "2.8.1", "2.8.2", "2.9.0", "2.9.0-1", "2.9.1", "2.9.1-1", "2.9.2"
+        "2.8.1", "2.8.2", "2.9.0", "2.9.0-1", "2.9.1", "2.9.1-1", "2.9.2", "2.10.0"
       )
     )
   )
+
+  def scalatestDependency(scalaVersion: String) = scalaVersion match {
+    case "2.10.0" => "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test"
+    case "2.8.1"  => "org.scalatest" %% "scalatest" % "1.8" % "test"
+    case "2.8.2"  => "org.scalatest" %% "scalatest" % "1.8" % "test"
+    case _ => "org.scalatest" %% "scalatest" % "1.9.1" % "test"
+  }
 }
