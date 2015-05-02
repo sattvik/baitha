b7c462cdfcf0925e240eca998844e1478aa6ccec
Daniel Solano Gómez <daniel@solanogomez.org>
2015-05-01 21:52:45 -0500

    Various updates to new IDEA/SBT/Scala versions

--- project/build.scala
+++ project/build.scala
@@ -1,38 +1,34 @@
 import sbt._
-
 import Keys._
-import AndroidKeys._
 
-object BaithaDefaults {
-  val settings =
-    Defaults.defaultSettings ++
-    AndroidBase.settings ++ Seq (
-      name := "Baitha: The Scala/Android Toolkit",
-      organization := "com.sattvik",
-      version := "0.1.1-SNAPSHOT",
-      scalaVersion := "2.10.0",
-      scalacOptions ++= Seq (
-        Opts.compile.deprecation,
-        Opts.compile.unchecked,
-        Opts.compile.optimise
-      ),
-      platformName in Android := "android-17"
+object BaithtaBuild extends Build {
+  val globalSettings = super.settings ++ Seq(
+    name := "Baitha: The Scala/Android Toolkit",
+    organization := "com.sattvik",
+    scalaVersion := "2.10.5",
+    javacOptions ++= Seq(
+      "-source", "1.6",
+      "-target", "1.6",
+      "-encoding", "UTF-8"
+    ),
+    scalacOptions ++= Seq (
+      Opts.compile.deprecation,
+      Opts.compile.unchecked,
+      Opts.compile.optimise
     )
-}
+  )
 
-object BaithaBuild extends Build {
   lazy val main = Project(
     "baitha",
     file("."),
-    settings = BaithaDefaults.settings ++ Seq (
+    settings = globalSettings ++ Seq (
       name := "baitha",
+      version := "0.1.1-SNAPSHOT",
       libraryDependencies ++= Seq (
-        "com.google.android" % "support-v4" % "r7",
-        "org.mockito" % "mockito-core" % "1.9.5" % "test",
-        "org.scalatest" %% "scalatest" % "1.9.1" % "test"
-      ),
-      crossScalaVersions := Seq(
-        "2.9.0", "2.9.0-1", "2.9.1", "2.9.1-1", "2.9.2", "2.10.0"
+        "com.google.android" % "android" % "4.1.1.4" % "provided",
+        "com.google.android" % "support-v4" % "r7" % "provided",
+        "org.mockito" % "mockito-core" % "2.0.7-beta" % "test",
+        "org.scalatest" %% "scalatest" % "2.2.1" % "test" exclude("org.scala-lang", "scala-reflect")
       )
     )
   )
